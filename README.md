72581a60d51eeb2b1f1d81be23f3b8002ab89ad6
Bob Roberts <the.bobcomp@gmail.com>
2014-02-14 11:18:14 -0500

    Added some simple instructions to readme

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ static-nodeku
 =============
 
 A light static file server for heroku based on Node.js
+
+Quick Start Guide
+-----------------
+This is all you need to get your site out there.
+
+1. Sign up for a heroku account
+1. Download the heroku toolbelt
+1. Run `heroku login` and enter your credentials
+1. Clone static-nodeku
+1. Load your html, css and js into the site folder
+  + Make sure this matches the url structure you expect i.e. url/path/to/file.css
+1. Cd to the new directory and run `heroku create`
+1. Run `git add .`
+1. Run `git commit -a -m "MESSAGE"`
+1. Run `git push heroku master`
+1. Run `heroku ps:scale web=1`
--- server.js
+++ server.js
@@ -8,21 +8,22 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(request, response) {
-  var my_path = url.parse(request.url).pathname;  
-	if(my_path == '/'){
-		my_path = '/index.html';
+app.get('*', function(request, response) {
+  var urlPath = url.parse(request.url).pathname;  
+	if(urlPath == '/'){
+		urlPath = '/index.html';
 	}
 	
-	var full_path = path.join(process.cwd(),properties.webroot,my_path);  
-	filesys.exists(full_path,function(exists){  
+	var fsPath = path.join(process.cwd(),properties.webroot,urlPath); 
+	console.log("Resource path " + fsPath);	
+	filesys.exists(fsPath,function(exists){  
 		if(!exists){  
 			response.writeHeader(404, {"Content-Type": "text/plain"});    
 			response.write("404 Not Found\n");    
 			response.end();  
 		}  
 		else{  
-			filesys.readFile(full_path, "binary", function(err, file) {    
+			filesys.readFile(fsPath, "binary", function(err, file) {    
 				 if(err) {    
 					 response.writeHeader(500, {"Content-Type": "text/plain"});    
 					 response.write(err + "\n");    
