66d838df9c1b1a5d03b8804468a05922d8f029ca
Bob Roberts <the.bobcomp@gmail.com>
2014-02-15 08:18:23 -0500

    Refined the server to make beter use of expresses' built in static server

--- server.js
+++ server.js
@@ -1,44 +1,26 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var path = require("path");
-var url = require("url");
-var filesys = require("fs");
 var properties = require("./properties.js");
+
+var oneDay = 86400000;
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('*', function(request, response) {
-  var urlPath = url.parse(request.url).pathname;  
-	if(urlPath == '/'){
-		urlPath = '/index.html';
-	}
-	
-	var fsPath = path.join(process.cwd(),properties.webroot,urlPath); 
-	console.log("Resource path " + fsPath);	
-	filesys.exists(fsPath,function(exists){  
-		if(!exists){  
-			response.writeHeader(404, {"Content-Type": "text/plain"});    
-			response.write("404 Not Found\n");    
-			response.end();  
-		}  
-		else{  
-			filesys.readFile(fsPath, "binary", function(err, file) {    
-				 if(err) {    
-					 response.writeHeader(500, {"Content-Type": "text/plain"});    
-					 response.write(err + "\n");    
-					 response.end();    
-				 
-				 }    
-				 else{  
-					response.writeHeader(200);    
-					response.write(file, "binary");    
-					response.end();  
-				}  
-					   
-			});  
-		}  
-	}); 
+app.use(express.compress());
+
+app.use(express.static(path.join(__dirname,properties.webroot), { maxAge: oneDay }));
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.send('404 not found');
+});
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.send(err);
 });
 
 var port = Number(process.env.PORT || properties.defaultPort);
